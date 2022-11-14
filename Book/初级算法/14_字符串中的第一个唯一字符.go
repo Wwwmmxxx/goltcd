e51be5418d733685591de11abf52877645a30793
CFI-5M8TSVB15TI\DELL <yifei.xu@dignative.tech>
2022-11-14 14:43:26 +0800

    create 14 topic

---
+++ Book/初级算法/14_字符串中的第一个唯一字符.go
@@ -0,0 +1,26 @@
+package main
+
+/**
+
+给定一个字符串 s ，找到 它的第一个不重复的字符，并返回它的索引 。如果不存在，则返回 -1 。
+
+示例 1:
+	输入: s = "leetcode"
+	输出: 0
+
+示例 2:
+	输入: s = "loveleetcode"
+	输出: 2
+
+示例 3:
+	输入: s = "aabb"
+	输出: -1
+
+提示:
+	1. 1 <= s.length <= 105
+	2. s 只包含小写字母
+*/
+
+func firstUniqChar(s string) int {
+	return 0
+}
