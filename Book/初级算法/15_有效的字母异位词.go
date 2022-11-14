b6c83233397b6c2b932559bf61bee03e2f52f4d8
CFI-5M8TSVB15TI\DELL <yifei.xu@dignative.tech>
2022-11-14 14:49:26 +0800

    create 15 topic

---
+++ Book/初级算法/15_有效的字母异位词.go
@@ -0,0 +1,29 @@
+package main
+
+/**
+
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+
+示例 1:
+	输入: s = "anagram", t = "nagaram"
+	输出: true
+
+示例 2:
+	输入: s = "rat", t = "car"
+	输出: false
+
+提示:
+	1. 1 <= s.length, t.length <= 5 * 104
+	2. s 和 t 仅包含小写字母
+
+作者：力扣 (LeetCode)
+链接：https://leetcode.cn/leetbook/read/top-interview-questions-easy/xn96us/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+*/
+
+func isAnagram(s string, t string) bool {
+	return false
+}
