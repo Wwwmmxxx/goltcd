fe806eb9f038477c72dc752bcac9ef3e04bc1e99
xu.yifei <348377025@qq.com>
2022-11-24 22:45:35 +0800

    finish 18, create 19

--- Book/初级算法/18_实现strStr.go
+++ Book/初级算法/18_实现strStr.go
@@ -1,8 +1,10 @@
 package main
 
+import "fmt"
+
 /**
 
-给你两个字符串 haystack 和 needle, 请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）. 如果 needle 不是 haystack 的一部分, 则返回  -1 .
+给你两个字符串haystack 和 needle, 请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）. 如果 needle 不是 haystack 的一部分, 则返回 -1 .
 
 示例 1:
 	输入: haystack = "sadbutsad", needle = "sad"
@@ -26,5 +28,17 @@ package main
 */
 
 func strStr(haystack string, needle string) int {
-	return 0
+
+	n, m := len(haystack), len(needle)
+outer:
+	for i := 0; i+m <= n; i++ {
+		for j := range needle {
+			fmt.Println(j)
+			if haystack[i+j] != needle[j] {
+				continue outer
+			}
+		}
+		return i
+	}
+	return -1
 }
---
+++ Book/初级算法/18_实现strStr_test.go
@@ -0,0 +1,55 @@
+package main
+
+import "testing"
+
+func Test_strStr(t *testing.T) {
+	type args struct {
+		haystack string
+		needle   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test01",
+			args: args{
+				haystack: "sadbutsad",
+				needle:   "sad",
+			},
+			want: 0,
+		},
+		{
+			name: "test02",
+			args: args{
+				haystack: "leetcode",
+				needle:   "leeto",
+			},
+			want: -1,
+		},
+		{
+			name: "test03",
+			args: args{
+				haystack: "a",
+				needle:   "a",
+			},
+			want: 0,
+		},
+		{
+			name: "test04",
+			args: args{
+				haystack: "mississippi",
+				needle:   "issip",
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := strStr(tt.args.haystack, tt.args.needle); got != tt.want {
+				t.Errorf("strStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
