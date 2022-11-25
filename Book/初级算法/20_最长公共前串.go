3f220b7bb1722131f48868be16278b009a15515d
CFI-5M8TSVB15TI\DELL <yifei.xu@dignative.tech>
2022-11-25 14:21:59 +0800

    finish 19, create 20

---
+++ Book/初级算法/20_最长公共前串.go
@@ -0,0 +1,52 @@
+package main
+
+/**
+
+编写一个函数来查找字符串数组中的最长公共前缀。
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1：
+	输入：strs = ["flower","flow","flight"]
+	输出："fl"
+
+示例 2：
+	输入：strs = ["dog","racecar","car"]
+	输出：""
+	解释：输入不存在公共前缀。
+
+作者：力扣 (LeetCode)
+链接：https://leetcode.cn/leetbook/read/top-interview-questions-easy/xnmav1/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+**/
+
+func longestCommonPrefix(strs []string) string {
+
+	var (
+		ptrs = make([]int, 0, len(strs))
+	)
+
+	loopTimes := getShortestString(strs)
+
+	for index := 0; index < loopTimes; index++ {
+
+		b := strs[index][ptrs[index]]
+
+	}
+
+	return ""
+}
+
+func getShortestString(strs []string) int {
+
+	result := len(strs[0])
+
+	for _, v := range strs {
+		if len(v) < result {
+			result = len(v)
+		}
+	}
+
+	return result
+}
