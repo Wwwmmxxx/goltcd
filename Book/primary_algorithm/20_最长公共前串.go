8a7364078e36292d5d22b2a6e31bbc298515a6d1
CFI-5M8TSVB15TI\DELL <yifei.xu@dignative.tech>
2022-11-25 14:57:47 +0800

    finish 20

--- Book/primary_algorithm/20_最长公共前串.go
+++ Book/primary_algorithm/20_最长公共前串.go
@@ -23,8 +23,17 @@ package book01
 
 func longestCommonPrefix(strs []string) string {
 
-	//for _, str := range strs {
-	//	if
-	//}
-	return ""
+	if len(strs) == 0 {
+		return ""
+	}
+
+	for i := 0; i < len(strs[0]); i++ {
+		for j := 1; j < len(strs); j++ {
+			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
+				return strs[0][:i]
+			}
+		}
+	}
+
+	return strs[0]
 }
