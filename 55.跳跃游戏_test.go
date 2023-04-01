367961f2fb42cd93bfe2b104834988932a3d8e82
“Wwwmmmxx” <348377025@qq.com>
2023-04-01 19:19:05 +0800

    add 55

---
+++ 55.跳跃游戏_test.go
@@ -0,0 +1,30 @@
+package goLeetCode
+
+import "testing"
+
+func Test_canJump(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "success",
+			args: args{
+				nums: []int{2, 3, 1, 1, 4},
+				// nums: []int{2, 0},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canJump(tt.args.nums); got != tt.want {
+				t.Errorf("canJump() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
