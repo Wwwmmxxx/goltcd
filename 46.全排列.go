2dda299bd746e71f21ef635d9592da821c3cc0bd
yifei.xu <yifei.xu@dignative.tech>
2023-04-07 17:49:23 +0800

    add 46

---
+++ 46.全排列.go
@@ -0,0 +1,84 @@
+package goLeetCode
+
+/*
+ * @lc app=leetcode.cn id=46 lang=golang
+ *
+ * [46] 全排列
+ *
+ * https://leetcode.cn/problems/permutations/description/
+ *
+ * algorithms
+ * Medium (78.87%)
+ * Likes:    2505
+ * Dislikes: 0
+ * Total Accepted:    838.4K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [1]
+ * 输出：[[1]]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * nums 中的所有整数 互不相同
+ *
+ *
+ */
+
+// @lc code=start
+func permute(nums []int) [][]int {
+	res := [][]int{}
+	visited := map[int]bool{}
+
+	var dfs func(path []int)
+	dfs = func(path []int) {
+		if len(path) == len(nums) {
+			temp := make([]int, len(path))
+			copy(temp, path)
+			res = append(res, temp)
+			return
+		}
+		for _, n := range nums {
+			if visited[n] {
+				continue
+			}
+			path = append(path, n)
+			visited[n] = true
+			dfs(path)
+			path = path[:len(path)-1]
+			visited[n] = false
+		}
+	}
+
+	dfs([]int{})
+	return res
+}
+
+// @lc code=end
