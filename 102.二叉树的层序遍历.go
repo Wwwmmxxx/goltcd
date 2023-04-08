645ae25d9f08d2b6c4507ddbf0996578352f6809
“Wwwmmmxx” <348377025@qq.com>
2023-04-08 23:14:34 +0800

    add 102

---
+++ 102.二叉树的层序遍历.go
@@ -0,0 +1,107 @@
+package goLeetCode
+
+/*
+ * @lc app=leetcode.cn id=102 lang=golang
+ *
+ * [102] 二叉树的层序遍历
+ *
+ * https://leetcode.cn/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (65.52%)
+ * Likes:    1643
+ * Dislikes: 0
+ * Total Accepted:    787.6K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[9,20],[15,7]]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1]
+ * 输出：[[1]]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目在范围 [0, 2000] 内
+ * -1000 <= Node.val <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrder(root *TreeNode) [][]int {
+
+	if root == nil {
+		return nil
+	}
+
+	result := [][]int{
+		{root.Val},
+	}
+
+	var f func(dept int, nodes ...*TreeNode)
+
+	f = func(dept int, nodes ...*TreeNode) {
+		if len(nodes) == 0 {
+			return
+		}
+
+		var tmpNodes = make([]*TreeNode, 0)
+		var curDeptValue = make([]int, 0, len(nodes))
+		for i := 0; i < len(nodes); i++ {
+
+			if nodes[i].Left != nil {
+				curDeptValue = append(curDeptValue, nodes[i].Left.Val)
+				tmpNodes = append(tmpNodes, nodes[i].Left)
+			}
+			if nodes[i].Right != nil {
+				curDeptValue = append(curDeptValue, nodes[i].Right.Val)
+				tmpNodes = append(tmpNodes, nodes[i].Right)
+			}
+
+		}
+
+		if len(curDeptValue) > 0 {
+			result = append(result, curDeptValue)
+		}
+
+		f(dept+1, tmpNodes...)
+	}
+
+	f(0, root)
+
+	return result
+}
+
+// @lc code=end
---
+++ 165.比较版本号_test.go
@@ -0,0 +1,39 @@
+package goLeetCode
+
+import "testing"
+
+func Test_compareVersion(t *testing.T) {
+	type args struct {
+		version1 string
+		version2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "03",
+			args: args{
+				version1: "1.0.1",
+				version2: "1",
+			},
+			want: 1,
+		},
+		{
+			name: "80",
+			args: args{
+				version1: "19.8.3.17.5.01.0.0.4.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0000.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.000000",
+				version2: "19.8.3.17.5.01.0.0.4.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0000.0.0.0.0",
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := compareVersion(tt.args.version1, tt.args.version2); got != tt.want {
+				t.Errorf("compareVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
