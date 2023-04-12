8c9431277f16ecffae3d14e060682b504d7adb9b
yifei.xu <yifei.xu@dignative.tech>
2023-04-12 11:51:01 +0800

    add 56

---
+++ 56.合并区间.go
@@ -0,0 +1,110 @@
+package goLeetCode
+
+import "sort"
+
+/*
+ * @lc app=leetcode.cn id=56 lang=golang
+ *
+ * [56] 合并区间
+ *
+ * https://leetcode.cn/problems/merge-intervals/description/
+ *
+ * algorithms
+ * Medium (49.28%)
+ * Likes:    1860
+ * Dislikes: 0
+ * Total Accepted:    614.9K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
+ *
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi]
+ * 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * 输出：[[1,6],[8,10],[15,18]]
+ * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：intervals = [[1,4],[4,5]]
+ * 输出：[[1,5]]
+ * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= intervals.length <= 10^4
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+func merge(intervals [][]int) [][]int {
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	var (
+		result = make([][]int, 0)
+	)
+
+	max := func(i int, j int) int {
+		if i > j {
+			return i
+		}
+		return j
+	}
+
+	for _, interval := range intervals {
+
+		length := len(result)
+
+		if len(result) == 0 {
+			result = append(result, interval)
+		} else if result[length-1][1] < interval[0] {
+			result = append(result, interval)
+		} else {
+			result[length-1][1] = max(result[length-1][1], interval[1])
+		}
+	}
+	return result
+}
+
+// @lc code=end
+
+// func merge1(intervals [][]int) [][]int {
+// 	//先从小到大排序
+// 	sort.Slice(intervals, func(i, j int) bool {
+// 		return intervals[i][0] < intervals[j][0]
+// 	})
+
+// 	max := func(i int, j int) int {
+// 		if i > j {
+// 			return i
+// 		}
+// 		return j
+// 	}
+
+// 	var res [][]int
+// 	for _, interval := range intervals {
+// 		var length = len(res)
+// 		if length == 0 || res[length-1][1] < interval[0] {
+// 			res = append(res, interval)
+// 		} else {
+// 			res[length-1][1] = max(res[length-1][1], interval[1])
+// 		}
+// 	}
+// 	return res
+// }
---
+++ 56.合并区间_test.go
@@ -0,0 +1,65 @@
+package goLeetCode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_merge(t *testing.T) {
+	type args struct {
+		intervals [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "case01",
+			args: args{
+				intervals: [][]int{
+					{1, 3},
+					{2, 6},
+					{8, 10},
+					{15, 18},
+				},
+			},
+			want: [][]int{
+				{1, 6},
+				{8, 10},
+				{15, 18},
+			},
+		},
+		{
+			name: "case02",
+			args: args{
+				intervals: [][]int{
+					{1, 4},
+					{2, 3},
+				},
+			},
+			want: [][]int{
+				{1, 4},
+			},
+		},
+		{
+			name: "case03",
+			args: args{
+				intervals: [][]int{
+					{1, 4},
+					{1, 4},
+				},
+			},
+			want: [][]int{
+				{1, 4},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := merge(tt.args.intervals); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("merge() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
