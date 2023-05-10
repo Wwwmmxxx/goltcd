d8a3a0a71c161cfef6baa4200db49004e1b1f20c
yifei.xu <yifei.xu@dignative.tech>
2023-05-10 11:58:59 +0800

    update no.33

--- part1/33.搜索旋转排序数组.go
+++ part1/33.搜索旋转排序数组.go
@@ -32,6 +32,30 @@ package goltcd
 
 // @lc code=start
 func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] >= nums[left] {
+			// 若左边是递增的
+			if nums[mid] > target && target >= nums[left] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			// 右边是递增的
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+
 	return -1
 }
 
--- part4/162.寻找峰值.go
+++ part4/162.寻找峰值.go
@@ -90,21 +90,19 @@ func findPeakElement(nums []int) int {
 }
 
 func findPeakElement1(nums []int) int {
-	n := len(nums)
-
-	get := func(i int) int {
-		if i == -1 || i == n {
+	f := func(i int) int {
+		if i == -1 || i == len(nums) {
 			return math.MinInt64
 		}
 		return nums[i]
 	}
+	left, right := 0, len(nums)
 
-	left, right := 0, n-1
 	for {
 		mid := (left + right) / 2
-		if get(mid) > get(mid-1) && get(mid) > get(mid+1) {
+		if f(mid) > f(mid-1) && f(mid) > f(mid+1) {
 			return mid
-		} else if get(mid) < get(mid+1) {
+		} else if f(mid) < f(mid+1) {
 			left = mid + 1
 		} else {
 			right = mid - 1
